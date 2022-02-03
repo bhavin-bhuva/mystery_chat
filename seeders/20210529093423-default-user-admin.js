d45b821e2b101908c5cb6d6bb9495aab171a04bb
Bhavin Bhuva <bhavin.b@heyarrow.com>
2022-02-03 21:37:25 +0530

    implemented seeders

---
+++ seeders/20210529093423-default-user-admin.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const role = await queryInterface.bulkInsert(
+      'roles',
+      [
+        {
+          name: 'admin',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'user',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      { returning: true }
+    );
+    let roleId = null;
+
+    for (const roleElement of role) {
+      if (roleElement.name == 'admin') {
+        roleId = roleElement.id;
+      }
+    }
+
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          first_name: 'admin',
+          last_name: 'admin',
+          email: 'admin@santokibate.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: roleId || null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
---
+++ seeders/create-bulk-admins.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          first_name: 'Herman',
+          last_name: 'Gomez',
+          email: 'hermangomez@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Callie',
+          last_name: 'Jones',
+          email: 'callie@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Earl',
+          last_name: 'Cross',
+          email: 'earl@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Frances',
+          last_name: 'Burns',
+          email: 'frances@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Mario',
+          last_name: 'Cox',
+          email: 'mario@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Christine',
+          last_name: 'Johnson',
+          email: 'christine@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Adelaide',
+          last_name: 'Sherman',
+          email: 'adelaide@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          first_name: 'Hulda',
+          last_name: 'Curtis',
+          email: 'hulda@mysterychat.com',
+          password: bcrypt.hashSync('sample', 10),
+          role_id: 2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
